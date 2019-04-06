659b56c420eeda21f452c9a5c2ba13821ddc2a01
Matheus Ricelly <fsenaweb@gmail.com>
2019-04-06 19:25:30 -0300

    Atualização do Perfil

--- src/components/sobre/Index.vue
+++ src/components/sobre/Index.vue
@@ -14,21 +14,52 @@
       <div class="body">
         <div class="content">
           <div class="column">
+            <div class="circulo"></div>
             <h1>fsenaweb<span class="me">.me</span></h1>
-            <h2>Especializado em desenvolver soluções web para o sucesso dos seus negócios</h2>
+            <h2>Desenvolvendo soluções web para o sucesso dos seus negócios</h2>
+            <div class="ferramentas">
+              <h3>FORMAÇÃO</h3>
+              <h4>Graduando em tecnologia em Sistemas para Internet pela Universidade Potiguar – UNP</h4>
+              <br>
+              <h3>COMPETÊNCIAS PESSOAIS</h3>
+              <h4>Responsabilidade<br>
+                Espírito de Equipe<br>
+                Dedicação<br>
+                Organização<br>
+              </h4>
+
+            </div>
           </div>
           <div class="column">
-          Muito prazer, meu nome é Matheus Ricelly, sou da cidade de Pau dos Ferros, no interior do Rio Grande do Norte. E como desenvolvedor web, tenho mais de 8 anos de experiência utilizando a linguagem PHP em meus projetos. Trabalho com ferramentas como Laravel, CodeIgniter, Lumen, Git, Docker, entre outras. <br><br>
+            <p>Muito prazer, meu nome é Matheus Ricelly, sou da cidade de Pau dos Ferros, no interior do Rio Grande do
+              Norte. E como desenvolvedor web, tenho mais de 8 anos de experiência utilizando a linguagem PHP em meus
+              projetos. Trabalho com ferramentas como Laravel, CodeIgniter, Lumen, Git, Docker, entre outras.</p>
 
-          Nos meus projetos desenvolvidos, tenho soluções completas como Progressive Web App (PWA), Sites, Sistemas Web, Landing Page, Single Page Application (SPA) e aplicações Mobile (Android e iOS). <br><br>
+            <p>Nos meus projetos desenvolvidos, tenho soluções completas como Progressive Web App (PWA), Sites, Sistemas
+              Web, Landing Page, Single Page Application (SPA) e aplicações Mobile (Android e iOS).</p>
 
-            Sou entusiasta do mundo javascript e venho fortalecendo meus conhecimentos com o Node, Express, Adonis.js, além de ferramentas frontend como Vue e React para deixar as aplicações web e mobile ainda mais interativas. <br><br>
+            <p>Sou entusiasta do mundo javascript e venho fortalecendo meus conhecimentos com o Node, Express,
+              Adonis.js, além de ferramentas frontend como Vue e React para deixar as aplicações web e mobile ainda mais
+              interativas.</p>
+            <div class="ferramentas">
+              <div class="column">
+                <h3>HABILIDADES</h3>
+                <h4>HTML5/CSS3</h4>
+                <h5>
+                  <progress-bar bar-color="#dc720f" val="90" text="90%" title="90%"></progress-bar>
+                </h5>
+                <h4>JS</h4>
+                <h5>
+                  <progress-bar bar-color="#dc720f" val="65" text="65%" title="65%"></progress-bar>
+                </h5>
+                <h4>Designer / Wireframes</h4>
+                <h5>
+                  <progress-bar bar-color="#dc720f" val="70" text="70%" title="70%"></progress-bar>
+                </h5>
+              </div>
+            </div>
 
-          <!--A MISSÃO é entregar soluções completas aos clientes, investindo em alta tecnologia para obter excelentes retornos ao contínuo crescimento das empresas. Sempre oferecendo soluções que potencializem os negócios e atendam as necessidades e desejos dos clientes, desenvolvendo projetos bonitos e produtivos. <br><br>-->
-
-          <!--A VISÃO é de estar sempre evoluindo e conquistando os maiores patamares na área de desenvolvimento web e mobile, visando a crescente expansão dos negócios e mercados, buscando as tendências que elas oferecem.-->
-            <!--<br><br>-->
-            Conheça as minhas redes sociais, veja meus trabalhos e participe também dos projetos open-source.
+            <p>Conheça as minhas redes sociais, veja meus trabalhos e participe também dos projetos open-source.</p>
           </div>
         </div>
       </div>
@@ -43,106 +74,117 @@
 </template>
 
 <script>
-import Menu from '@/components/layout/Menu'
-export default {
-  name: 'Portfolio',
-  data () {
-    return {
-      loading: true,
-      move: false
-    }
-  },
-  head: {
-    title: {
-      inner: 'Quem Somos'
+  import Menu from '@/components/layout/Menu'
+  import ProgressBar from 'vue-simple-progress'
+
+  export default {
+    name: 'Portfolio',
+    data () {
+      return {
+        loading: true,
+        move: false
+      }
     },
-    // Meta tags
-    meta: [
-      {
-        name: 'description',
-        content: 'Experiência em desenvolvimento de projetos para web em todos os segmentos',
-        id: 'desc'
+    head: {
+      title: {
+        inner: 'Quem Somos'
       },
-      // Twitter
-      {
-        name: 'twitter:title',
-        content: 'fsenaweb.me'
-      },
-      // with shorthand
-      {
-        n: 'twitter:description',
-        c: 'fsenaweb.me - Quem Somos - Experiência em desenvolvimento de projetos para web em todos os segmentos'
-      },
-      {
-        property: 'og:title',
-        content: 'fsenaweb.me - Quem Somos - Experiência em desenvolvimento de projetos para web em todos os segmentos'
-      }
-    ],
-    // link tags
-    link: [
-      {
-        rel: 'canonical',
-        href: 'http://fsenaweb.me/#/sobre/',
-        id: 'canonical'
-      },
-      {
-        rel: 'icon',
-        href: '/static/assets/favicon-16.png',
-        sizes: '16x16',
-        type: 'image/png'
-      },
-      {
-        rel: 'icon',
-        href: '/static/assets/favicon-32.png',
-        sizes: '32x32',
-        type: 'image/png'
-      }
-    ]
-  },
-  components: {
-    Menu
-  },
-  mounted () {
-    this.loading = false
-    this.move = true
+      // Meta tags
+      meta: [
+        {
+          name: 'description',
+          content: 'Experiência em desenvolvimento de projetos para web em todos os segmentos',
+          id: 'desc'
+        },
+        // Twitter
+        {
+          name: 'twitter:title',
+          content: 'fsenaweb.me'
+        },
+        // with shorthand
+        {
+          n: 'twitter:description',
+          c: 'fsenaweb.me - Quem Somos - Experiência em desenvolvimento de projetos para web em todos os segmentos'
+        },
+        {
+          property: 'og:title',
+          content: 'fsenaweb.me - Quem Somos - Experiência em desenvolvimento de projetos para web em todos os segmentos'
+        }
+      ],
+      // link tags
+      link: [
+        {
+          rel: 'canonical',
+          href: 'http://fsenaweb.me/#/sobre/',
+          id: 'canonical'
+        },
+        {
+          rel: 'icon',
+          href: '/static/assets/favicon-16.png',
+          sizes: '16x16',
+          type: 'image/png'
+        },
+        {
+          rel: 'icon',
+          href: '/static/assets/favicon-32.png',
+          sizes: '32x32',
+          type: 'image/png'
+        }
+      ]
+    },
+    components: {
+      Menu, ProgressBar
+    },
+    mounted () {
+      this.loading = false
+      this.move = true
+    }
   }
-}
 </script>
 
 <style scoped>
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+
   .topo {
     display: flex;
     flex-direction: column;
     width: 100vw;
-    height: 50vh;
+    height: 45vh;
     background: url("/static/assets/bckgr4.jpg");
     background-attachment: fixed;
-    background-position: 50% 77%;
+    background-position: center top;
     background-repeat: no-repeat;
-    background-size: 100vw;
+    background-size: 130vw;
     justify-content: center;
     align-items: center;
     text-align: center;
+    border: 1px solid #000;
   }
+
   .topo h1 {
     width: 85%;
     display: block;
     color: #fff;
     font-size: 3.5vmax;
   }
+
   .topo h6 {
     color: #fff;
     font-size: 1.5vmax;
   }
+
   .loading {
     display: flex;
     width: 98vw;
     height: 97vh;
-    background-color: rgba(255,255,255,0.3);
+    background-color: rgba(255, 255, 255, 0.3);
     justify-content: center;
     align-items: center;
     color: #000;
   }
+
   .body {
     display: flex;
     flex-direction: column;
@@ -151,11 +193,13 @@ export default {
     background-color: #FFF;
     margin: 0 auto;
   }
+
   .body h1 {
     width: 100%;
     font-size: 1.4em;
     margin: 4% 2%;
   }
+
   .content {
     display: flex;
     flex-direction: row;
@@ -165,23 +209,47 @@ export default {
     font-size: 1.3em;
     color: #555;
   }
+
   .column:nth-child(1) {
     width: 35%;
     display: flex;
     flex-direction: column;
     padding: 1%;
   }
+
+  .circulo {
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    border: 10px solid #105e56;
+    margin: 0 auto;
+    background: url("../../assets/foto-perfil.jpg");
+    background-repeat: no-repeat;
+    background-position: -50px -10px;
+    background-size: 140% 140%;
+  }
+
+  .column:nth-child(1) img {
+    width: 50%;
+    height: 35%;
+    margin: 0 auto;
+    border: 10px solid #105e56;
+    border-radius: 100%;
+  }
+
   .column {
     width: 55%;
     padding: 1%;
   }
+
   .column h1 {
     font-size: 3em;
     padding: 0;
     margin: 0;
     color: #20b2aa;
-    margin-top: 20%;
+    margin-top: 10%;
   }
+
   .column h2 {
     font-size: 1.1em;
     line-height: 1em;
@@ -189,6 +257,7 @@ export default {
     margin: 0;
     color: #646464;
   }
+
   .column h1 .me {
     font-size: inherit;
     padding: 0;
@@ -196,14 +265,45 @@ export default {
     color: #105e56;
     margin-top: 20%;
   }
+
+  .column p {
+    margin-bottom: 35px;
+  }
+
+  .ferramentas {
+    width: 100%;
+    min-height: 150px;
+    margin: 25px 0;
+    padding: 15px;
+    border-radius: 10px;
+    background: url('../../assets/dotted.png');
+    background-size: 3.5px;
+    background-repeat: repeat;
+  }
+
+  .ferramentas h3 {
+    font-size: 90%;
+    margin: 15px 0;
+    padding-bottom: 7px;
+    border-bottom: 1px solid #CCC;
+  }
+
+  .ferramentas h4 {
+    font-size: 80%;
+    color: black;
+    margin-bottom: 10px;
+  }
+
   .fade-enter-active, .fade-leave-active {
-    transition: height .5s;
+    transition: height .4s;
     transition-timing-function: linear;
-    transition-delay: 1s;
+    transition-delay: 0.6s;
   }
+
   .fade-enter, .fade-leave-to {
-    height: 68vh;
+    height: 60vh;
   }
+
   .footer {
     display: flex;
     flex-direction: column;
@@ -218,6 +318,7 @@ export default {
     align-content: center;
     align-self: center;
   }
+
   .invert {
     display: flex;
     flex-direction: column;
@@ -229,31 +330,37 @@ export default {
     text-align: center;
     line-height: 1.5em;
   }
+
   .invert h1 {
     color: #FFF;
     font-size: 1.5em;
     text-shadow: 2px 1px 1px #000;
   }
+
   .invert h2 {
     color: #FFF;
     font-size: 1em;
     font-weight: lighter;
     text-shadow: 2px 1px 1px #000;
   }
+
   .invert h2 a {
     color: #FFF;
     text-decoration: none;
   }
+
   .invert h3 {
     color: #FFF;
     font-size: 0.8em;
     font-weight: lighter;
     text-shadow: 2px 1px 1px #000;
   }
+
   .invert h3 a {
     color: #FFF;
     text-decoration: none;
   }
+
   @media screen and (max-width: 700px) {
     .topo {
       display: flex;
@@ -264,11 +371,12 @@ export default {
       background-attachment: fixed;
       background-position: 0px 0px;
       background-repeat: no-repeat;
-      background-size: 130vw 50vh;
+      background-size: 130vw;
       justify-content: center;
       align-items: center;
       text-align: center;
     }
+
     .topo h1 {
       width: 85%;
       display: block;
@@ -276,19 +384,23 @@ export default {
       font-size: 6vmax;
       padding-top: 8vh;
     }
+
     .topo h6 {
       font-size: 2.5vmax;
       color: #fff;
       padding: 1vw;
     }
+
     .body {
       margin-bottom: 10%;
     }
+
     .body h1 {
       width: 100%;
       font-size: 1.1em;
       margin: 4% 0%;
     }
+
     .content {
       display: flex;
       flex-direction: column;
@@ -298,16 +410,19 @@ export default {
       font-size: 1.3em;
       color: #555;
     }
+
     .column:nth-child(1) {
       width: 100%;
       display: flex;
       flex-direction: column;
       padding: 1%;
     }
+
     .column {
       width: 100%;
       padding: 1%;
     }
+
     .column h1 {
       font-size: 2em;
       padding: 0;
@@ -315,6 +430,7 @@ export default {
       color: #20b2aa;
       margin-top: 5%;
     }
+
     .column h2 {
       font-size: 0.9em;
       line-height: 1em;
@@ -323,6 +439,7 @@ export default {
       color: #646464;
       margin-bottom: 5%;
     }
+
     .column h1 .me {
       font-size: inherit;
       padding: 0;
@@ -330,14 +447,17 @@ export default {
       color: #105e56;
       margin-top: 20%;
     }
+
     .fade-enter-active, .fade-leave-active {
       transition: height .5s;
       transition-timing-function: linear;
       transition-delay: 1s;
     }
+
     .fade-enter, .fade-leave-to {
       height: 60vh;
     }
+
     .footer {
       display: flex;
       flex-direction: column;
@@ -352,6 +472,7 @@ export default {
       align-content: center;
       align-self: center;
     }
+
     .invert {
       display: flex;
       flex-direction: column;
@@ -363,27 +484,32 @@ export default {
       text-align: center;
       line-height: 1.5em;
     }
+
     .invert h1 {
       color: #FFF;
       font-size: 1.2em;
       text-shadow: 2px 1px 1px #000;
     }
+
     .invert h2 {
       color: #FFF;
       font-size: 1em;
       font-weight: lighter;
       text-shadow: 2px 1px 1px #000;
     }
+
     .invert h2 a {
       color: #FFF;
       text-decoration: none;
     }
+
     .invert h3 {
       color: #FFF;
       font-size: 0.8em;
       font-weight: lighter;
       text-shadow: 2px 1px 1px #000;
     }
+
     .invert h3 a {
       color: #FFF;
       text-decoration: none;
